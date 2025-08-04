2cc707a5c7d6c9a69ed78509c048613a03c9a108
altagracia <samirasanchez8090@gmail.com>
2025-08-04 19:39:47 -0400

    template logic

--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
-from fastapi import FastAPI, HTTPException, Body 
+from fastapi import FastAPI, HTTPException, Request, Body 
 from databases import Database
 from sqlalchemy import *
 from models import Guide, Track, Base
+from schemas import GuideCreate, TrackCreate
+
+#template 
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 DATABASE_URL = "" 
 database = Database(DATABASE_URL)
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
 
 @app.on_event("startup")
 async def startup():
@@ -23,6 +32,33 @@ async def health():
     await database.execute("SELECT 1")
     return {"status": "ok"}
 
-#routes
+#routes for guides 
 @app.post("/guides/")
-async def post_audio(Guide)
+async def post_guide(payload: GuideCreate):
+    guide_data = {
+        "title": payload.title,
+        "description": payload.description,
+        "category": payload.category
+    }
+
+    guide_id = await database.execute(Guide.insert().values(**guide_data))
+
+    
+    return {"message": "guide saved", "guide_id": guide_id}
+
+@app.get("/guides/{id}", response_class=HTMLResponse)
+async def read_guide(request: Request, id: int):
+
+    query = Guide.select().where(Guide.c.id == id)
+    guide = await database.fetch_one(query)
+
+    if not guide:
+        raise HTTPException(status_code=404, detail="Item not found")
+    
+    return templates.TemplateResponse(
+        request=request,
+        name="guides.html",
+        context={"Guide": Guide}
+    )
+
+#routes for
